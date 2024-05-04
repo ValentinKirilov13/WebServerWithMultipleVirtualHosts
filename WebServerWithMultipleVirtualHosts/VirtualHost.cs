1e4dcc9cbccf5cc505b077d73846bb0d0b0ab866
Valentin Kirilov <valkilmaar@gmail.com>
2024-05-04 14:57:35 +0300

    Add VirtualHosts model

---
+++ WebServerWithMultipleVirtualHosts/VirtualHost.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+
+namespace WebServerWithMultipleVirtualHosts
+{
+    public class VirtualHost
+    {
+        public required string Name { get; set; }
+        public required string Port { get; set; }
+        public required string RootDir { get; set; }
+        public required string LogFile { get; set; }
+        public required ILogger Loger { get; set; }
+    }
+}
