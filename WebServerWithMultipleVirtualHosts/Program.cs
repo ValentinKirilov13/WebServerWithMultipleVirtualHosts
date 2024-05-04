d8324dc06a601ed60e914bb37464645a4b6b1ab2
Valentin Kirilov <valkilmaar@gmail.com>
2024-05-04 14:58:21 +0300

    Configure StartUp

--- WebServerWithMultipleVirtualHosts/Program.cs
+++ WebServerWithMultipleVirtualHosts/Program.cs
@@ -1,2 +1,13 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using WebServerWithMultipleVirtualHosts;
+
+var configuration = new ConfigurationBuilder()
+           .AddJsonFile(@"C:\Users\valki\Desktop\Programing\Project_For_Intern_C#\WebServerWithMultipleVirtualHosts\WebServerWithMultipleVirtualHosts\appSettings.json")
+           .Build();
+
+var webServer = new WebServer(configuration);
+webServer.Start();
+
+Console.WriteLine("Enter any key to exit");
+Console.ReadKey();
+
